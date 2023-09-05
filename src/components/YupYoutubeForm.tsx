118953eb201b93ce7c975e522bac8b72581a44af
Shourov Foisal <shourovfoisal@gmail.com>
2023-09-05 17:52:31 +0600

    Tested new features of hook form
    1. Yup
    2. Yup InferType
    3. defaultValues
    4. values (Current values)
    5. Updating the whole form using the "values" property
    6. Using events like onChange explicitly to perform custom operations (the username field)
    7. Setting type to the event objects

--- src/components/YupYoutubeForm.tsx
+++ src/components/YupYoutubeForm.tsx
@@ -1,66 +1,82 @@
-import { DevTool } from '@hookform/devtools';
-import { useForm } from 'react-hook-form'
-import { yupResolver } from "@hookform/resolvers/yup"
-import * as yup from "yup"
+import { DevTool } from "@hookform/devtools";
+import { useForm } from "react-hook-form";
+import type { SubmitHandler } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 const schema = yup.object({
-    username: yup.string().required("Username is required"),
-    email: yup.string().email("Email format is not valid").required("Email is required"),
-    channel: yup.string().required("Channel is required")
+	username: yup.string().required("Username is required"),
+	email: yup
+		.string()
+		.email("Email format is not valid")
+		.required("Email is required"),
+	channel: yup.string().required("Channel is required"),
 });
 
-type FormValueTypes = {
-    username: string;
-    email: string;
-    channel: string;
-}
+type FormValueType = yup.InferType<typeof schema>;
+
+let dv = {
+	username: "",
+	email: "",
+	channel: "",
+};
 
 const YupYoutubeForm = () => {
+	const form = useForm<FormValueType>({
+		defaultValues: dv,
+		resolver: yupResolver(schema),
+		values: dv,
+	});
 
-    const form = useForm<FormValueTypes>({
-        defaultValues: {
-          username: "",
-          email: "",
-          channel: ""
-        },
-        resolver: yupResolver(schema)
-    });
+	const {
+		control,
+		formState: { errors },
+		handleSubmit,
+		register,
+	} = form;
 
-    const { control, formState, handleSubmit, register } = form;
+	const submitForm: SubmitHandler<FormValueType> = (formValue, e) => {
+		console.log(e);
+		console.log(formValue);
+		dv = { ...dv, channel: "HelloThere" }; // changing the value by assigning new object
+	};
 
-    const { errors } = formState;
+	// using onChange event explicitly
+	const onNameChange = (e: React.SyntheticEvent<HTMLInputElement>) => {
+		console.log(e.currentTarget.value);
+		register("username").onChange(e);
+	};
 
-    const submitForm = (formValue: FormValueTypes) => {
-        console.log(formValue)
-    }
+	return (
+		<div>
+			<h2>Yup Youtube Form</h2>
+			<form onSubmit={handleSubmit(submitForm)} noValidate>
+				<div className="form-control">
+					<label htmlFor="username">Username</label>
+					<input
+						type="text"
+						id="username"
+						{...register("username")}
+						onChange={onNameChange}
+					/>
+					<p className="error">{errors.username?.message}</p>
+				</div>
+				<div className="form-control">
+					<label htmlFor="email">Email</label>
+					<input type="email" id="email" {...register("email")} />
+					<p className="error">{errors.email?.message}</p>
+				</div>
+				<div className="form-control">
+					<label htmlFor="channel">Channel</label>
+					<input type="text" id="channel" {...register("channel")} />
+					<p className="error">{errors.channel?.message}</p>
+				</div>
+				<button>Submit</button>
+			</form>
 
-  return (
-    <div>
-        <h2>Yup Youtube Form</h2>
+			<DevTool control={control} />
+		</div>
+	);
+};
 
-        <form onSubmit={handleSubmit(submitForm)} noValidate>
-            <div className="form-control">
-                <label htmlFor="username">Username</label>
-                <input type="text" id="username" {...register("username")} />
-                <p className="error">{errors.username?.message}</p>
-            </div>
-            <div className="form-control">
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" {...register("email")} />
-                <p className="error">{errors.email?.message}</p>
-            </div>
-            <div className="form-control">
-                <label htmlFor="channel">Channel</label>
-                <input type="text" id="channel" {...register("channel")} />
-                <p className="error">{errors.channel?.message}</p>
-            </div>
-            <button>Submit</button>
-        </form>
-
-        <DevTool control={control} />
-    </div>
-
-  )
-}
-
-export default YupYoutubeForm
+export default YupYoutubeForm;
