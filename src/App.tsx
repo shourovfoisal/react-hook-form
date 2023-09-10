9efa1bb20d9a10a9e9c1db2850187c96e33f5988
Shourov Foisal <shourovfoisal@gmail.com>
2023-09-10 16:54:16 +0600

    async function return result as defaultValue

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import "./App.css";
 import { Navbar } from "./components/Navbar";
 // import { YoutubeForm } from './components/YoutubeForm'
@@ -9,7 +9,14 @@ import { RefComponents } from "./components/RefComponents";
 import { UseControllerHook } from "./components/UseControllerHook";
 import { FormComponent } from "./components/FormComponent";
 
+interface Person {
+	age: number;
+	hairColor: "black" | "brown" | "blonde";
+}
+
 function App() {
+	const [person, setPerson] = useState({});
+
 	return (
 		<Fragment>
 			<Navbar />
--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -1,68 +1,102 @@
 import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 
-
 type FormValues = {
-  username: string,
-  email: string,
-  channel: string
-}
-
+	username: string;
+	email: string;
+	channel: string;
+};
 
 export const YoutubeForm = () => {
+	const getFormDefaultValues = (): Promise<FormValues> => {
+		return new Promise(resolve => {
+			const formValues = {
+				username: "shourov",
+				email: "shourov@foisal.com",
+				channel: "cShourov",
+			};
+			setTimeout(() => {
+				resolve(formValues);
+			}, 1000);
+		});
+	};
 
-  const form = useForm<FormValues>();
-  const {register, control, handleSubmit, formState} = form;
-  const {errors} = formState;
+	const form = useForm<FormValues>({
+		// defaultValues: () => getFormDefaultValues(),
+		defaultValues: async () => await getFormDefaultValues(),
+	});
+	const { register, control, handleSubmit, formState } = form;
+	const { errors } = formState;
 
-  const onSubmit = (data: FormValues) => {
-    console.log("form submitted");
-    console.log(data);
-  }
+	const onSubmit = (data: FormValues) => {
+		console.log("form submitted");
+		console.log(data);
+	};
 
-  return (
-    <div>
-        {/* <form onSubmit={handleSubmit(onSubmit)} noValidate> */}
-            <div className="form-control">
-              <label htmlFor="username">Username</label>
-              <input type="text" id="username" {...register("username", {
-                required: "username is required."
-              })} />
-              <p className="error">{errors.username?.message}</p>
-            </div>
+	return (
+		<div>
+			{/* <form onSubmit={handleSubmit(onSubmit)} noValidate> */}
+			<div className="form-control">
+				<label htmlFor="username">Username</label>
+				<input
+					type="text"
+					id="username"
+					{...register("username", {
+						required: "username is required.",
+					})}
+				/>
+				<p className="error">{errors.username?.message}</p>
+			</div>
 
-            <div className="form-control">
-              <label htmlFor="email">Email</label>
-              <input type="email" id="email" {...register("email", {
-                pattern: {
-                  value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-                  message: "email is invalid."
-                },
-                validate: {
-                  canNotBeAdmin: (fieldValue) => {
-                    return fieldValue !== "admin@example.com" || "can not enter that email."
-                  },
-                  blacklistedWebsite: (fieldValue) => {
-                    return !fieldValue.endsWith("baddomain.com") || "that is not a good website."
-                  }
-                }
-              })} />
-              <p className="error">{errors.email?.message}</p>
-            </div>
+			<div className="form-control">
+				<label htmlFor="email">Email</label>
+				<input
+					type="email"
+					id="email"
+					{...register("email", {
+						pattern: {
+							value:
+								/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+							message: "email is invalid.",
+						},
+						validate: {
+							canNotBeAdmin: fieldValue => {
+								return (
+									fieldValue !== "admin@example.com" ||
+									"can not enter that email."
+								);
+							},
+							blacklistedWebsite: fieldValue => {
+								return (
+									!fieldValue.endsWith("baddomain.com") ||
+									"that is not a good website."
+								);
+							},
+						},
+					})}
+				/>
+				<p className="error">{errors.email?.message}</p>
+			</div>
 
-            <div className="form-control">
-              <label htmlFor="channel">Channel</label>
-              <input type="text" id="channel" {...register("channel", {
-                required: "channel is required."
-              })} />
-              <p className="error">{errors.channel?.message}</p>
-            </div>
+			<div className="form-control">
+				<label htmlFor="channel">Channel</label>
+				<input
+					type="text"
+					id="channel"
+					{...register("channel", {
+						required: "channel is required.",
+					})}
+				/>
+				<p className="error">{errors.channel?.message}</p>
+			</div>
 
-            {/* <button>Submit</button> */}
-            <button type="button" onClick={handleSubmit(onSubmit)}>Submit</button>
-        {/* </form> */}
+			{/* <button>Submit</button> */}
+			<button type="button" onClick={handleSubmit(onSubmit)}>
+				Submit
+			</button>
+			{/* </form> */}
 
-        <DevTool control={control} />
-    </div>
-  )
-}
+			<DevTool control={control} />
+		</div>
+	);
+};
